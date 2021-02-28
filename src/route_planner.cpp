500b18fe8cd5bb0119cb5caad9aa78618bb5b6b7
Hidenori Shinohara <hidenori@stellar.org>
2021-02-28 11:58:51 -0500

    Finish todo6

--- src/route_planner.cpp
+++ src/route_planner.cpp
@@ -75,11 +75,17 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     distance = 0.0f;
     std::vector<RouteModel::Node> path_found;
 
-    // TODO: Implement your solution here.
+    while (current_node != start_node) {
+        path_found.push_back(*current_node);
+        RouteModel::Node *parent = current_node->parent;
+        distance += current_node->distance(*parent);
+        current_node = parent;
+    }
+    path_found.push_back(*start_node);
+    std::reverse(path_found.begin(), path_found.end());
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;
-
 }
 
 
